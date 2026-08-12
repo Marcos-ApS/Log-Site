301a7624ebae9a165fc530f2f950f99bad83224d
Marcos-ApS <cuscuz1fx@gmail.com>
2026-08-11 21:05:15 -0300

    divisão do css & otimização

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+
+import "./style/global.css";
+import "./style/sidebar.css";
+import "./style/navbar.css";
+import "./style/dashboard.css";
+import "./style/login.css";
+import "./style/responsividade.css";
+import "./style/tabela.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
